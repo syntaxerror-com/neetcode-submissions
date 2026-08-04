11995709c48cd4313181fe79400c18c2e8101194
syntaxerror-com <mr.shaswatdas@gmail.com>
2026-08-04 21:55:42 +0530

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int>q;
+        vector<int>result;
+        for(int i=0;i<nums.size();i++){
+            while(!q.empty()&& q.front()<=i-k){
+                q.pop_front();
+            }
+            while(!q.empty()&& nums[q.back()]<nums[i]){
+                q.pop_back();
+            }
+            q.push_back(i);
+
+            if(i>=k-1){
+                result.push_back(nums[q.front()]);;
+            }
+        }
+      return result;
+    }
+};
